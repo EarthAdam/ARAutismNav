ff6b780922e38a2dd24495d14d38713ddfa9faeb
Kholstein <40220033+Kholstein@users.noreply.github.com>
2019-03-12 12:31:13 -0400

    Added AWS

---
+++ AR Core Navigator/Assets/AWS Assets/Assets/Examples/AppManager.cs
@@ -0,0 +1,124 @@
+﻿using Amazon.S3.Model;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+namespace AWSSDK.Examples
+{
+    public class AppManager : MonoBehaviour
+    {
+        public string S3BucketName;
+        public string fileNameOnBucket;
+        private string pathFileUpload;
+        public Button buttonUploadFileBucket;
+        public Text resultTextOperation;
+        public InputField UploadInputField;
+
+        void Start()
+        {
+            /* 
+            StreamWriter writer = new StreamWriter(Application.persistentDataPath+@"/"+System.DateTime.Now.ToString("yy.MM.dd")+".txt", true);
+            fileNameOnBucket = System.DateTime.Now.ToString("yy.MM.dd")+".txt";
+            writer.WriteLine("goofy");
+            writer.Close();
+            */
+            pathFileUpload = (Application.persistentDataPath+@"/"+System.DateTime.Now.ToString("yy.MM.dd")+".txt");
+            S3Manager.Instance.OnResultGetObject += GetObjectBucket;
+        }
+
+        void Update()
+        {
+            fileNameOnBucket = UploadInputField.text+".txt";    
+        }
+        public void ListObjectsBucket()
+        {
+            resultTextOperation.text = "Fetching all the Objects from " + S3BucketName;
+
+            S3Manager.Instance.ListObjectsBucket(S3BucketName, (result, error) =>
+            {
+                resultTextOperation.text += "\n";
+                if (string.IsNullOrEmpty(error))
+                {
+                    resultTextOperation.text += "Got Response \nPrinting now \n";
+                    result.S3Objects.ForEach((o) =>
+                    {
+                        resultTextOperation.text += string.Format("File: {0}\n", o.Key);
+                    });
+                }
+                else
+                {
+                    print("Get Error:: " + error);
+                    resultTextOperation.text += "Got Exception \n";
+                }
+            });
+        }
+
+        public void GetObjectBucket(GetObjectResponse resultFinal = null, string errorFinal = null)
+        {
+            resultTextOperation.text = string.Format("fetching {0} from bucket {1}", System.DateTime.Now.ToString("yy.MM.dd")+".txt", S3BucketName);
+            
+            if(errorFinal != null)
+            {
+                resultTextOperation.text += "\n";
+                resultTextOperation.text += "Get Data Error";
+                print("Get Error:: " + errorFinal);
+                return;
+            }
+            
+
+            S3Manager.Instance.GetObjectBucket(S3BucketName, System.DateTime.Now.ToString("yy.MM.dd")+".txt", (result, error) =>
+            {
+                if (string.IsNullOrEmpty(error))
+                {
+                    resultTextOperation.text += "\nGet Data Complete.";
+                }
+                else
+                {
+                    resultTextOperation.text += "\n";
+                    resultTextOperation.text += "Get Data Error";
+                    print("Get Error:: " + error);
+                }
+            });
+
+        }
+
+        public void UploadObjectForBucket()
+        {
+            resultTextOperation.text = "Retrieving the file";
+            resultTextOperation.text += "\nCreating request object";
+            resultTextOperation.text += "\nMaking HTTP post call";
+            string pathFile = Application.persistentDataPath+@"/"+UploadInputField.text +".txt";
+
+            S3Manager.Instance.UploadObjectForBucket(pathFile, S3BucketName, fileNameOnBucket, (result, error) =>
+            {
+                if(string.IsNullOrEmpty(error))
+                {
+                    resultTextOperation.text += "\nUpload Success";
+                }
+                else
+                {
+                    resultTextOperation.text += "\nUpload Failed";
+                    Debug.LogError("Get Error:: " + error);
+                }
+            });
+        }
+
+        public void DeleteObjectOnBucket()
+        {
+            resultTextOperation.text = string.Format("deleting {0} from bucket {1}\n", fileNameOnBucket, S3BucketName);
+
+            S3Manager.Instance.DeleteObjectOnBucket(fileNameOnBucket, S3BucketName, (result, error) =>
+            {
+                if (string.IsNullOrEmpty(error))
+                {
+                    resultTextOperation.text += "\nFile Deleted Success";
+                }
+                else
+                {
+                    resultTextOperation.text += "\nFile Deleted Failed";
+                    print("Get Error:: " + error);
+                }
+            });
+        }
+    }
+}
---
+++ AR Core Navigator/Assets/AWS Assets/Assets/Examples/S3Manager.cs
@@ -0,0 +1,234 @@
+//
+// Copyright 2014-2015 Amazon.com, 
+// Inc. or its affiliates. All Rights Reserved.
+// 
+// Licensed under the AWS Mobile SDK For Unity 
+// Sample Application License Agreement (the "License"). 
+// You may not use this file except in compliance with the 
+// License. A copy of the License is located 
+// in the "license" file accompanying this file. This file is 
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, express or implied. See the License 
+// for the specific language governing permissions and 
+// limitations under the License.
+//
+
+using UnityEngine;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.Runtime;
+using System.IO;
+using System.Collections.Generic;
+using Amazon.CognitoIdentity;
+using Amazon;
+using System;
+
+namespace AWSSDK.Examples
+{
+    public class S3Manager : MonoBehaviour
+    {
+        #region VARIABLES
+
+        public static S3Manager Instance { get; set; }
+
+        [Header("AWS Setup")]
+        [SerializeField] private string identityPoolId;
+        [SerializeField] private string cognitoIdentityRegion = RegionEndpoint.USEast2.SystemName;
+        [SerializeField] private string s3Region = RegionEndpoint.USEast2.SystemName;
+
+        // Variables privates
+        private int timeoutGetObject = 60; // seconds
+        private string resultTimeout = "";
+        public Action<GetObjectResponse, string> OnResultGetObject;
+        private IAmazonS3 s3Client;
+        private AWSCredentials credentials;
+
+        // Propertys
+        private RegionEndpoint CognitoIdentityRegion
+        {
+            get { return RegionEndpoint.GetBySystemName(cognitoIdentityRegion); }
+        }
+        private RegionEndpoint S3Region
+        {
+            get { return RegionEndpoint.GetBySystemName(s3Region); }
+        }
+        private AWSCredentials Credentials
+        {
+            get
+            {
+                if (credentials == null)
+                    credentials = new CognitoAWSCredentials(identityPoolId, CognitoIdentityRegion);
+                return credentials;
+            }
+        }
+        private IAmazonS3 Client
+        {
+            get
+            {
+                if (s3Client == null)
+                {
+                    s3Client = new AmazonS3Client(Credentials, S3Region);
+                }
+                //test comment
+                return s3Client;
+            }
+        }
+
+        #endregion
+
+        #region METHODS MONOBEHAVIOUR
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        void Start()
+        {
+            UnityInitializer.AttachToGameObject(this.gameObject);
+            AWSConfigs.HttpClient = AWSConfigs.HttpClientOption.UnityWebRequest;
+        }
+
+
+        #endregion
+
+        #region METHODS AWS SDK S3
+
+        /// <summary>
+        /// Get Objects from S3 Bucket
+        /// </summary>
+        public void ListObjectsBucket(string nameBucket, Action<ListObjectsResponse, string> result)
+        {
+            var request = new ListObjectsRequest()
+            {
+                BucketName = nameBucket
+            };
+
+            Client.ListObjectsAsync(request, (responseObject) =>
+            {
+                if (responseObject.Exception == null)
+                    result.Invoke(responseObject.Response, "");
+                else
+                    result.Invoke(null, responseObject.Exception.ToString());
+            });
+        }
+
+        /// <summary>
+        /// Get Object from S3 Bucket
+        /// </summary>
+        public void GetObjectBucket(string S3BucketName, string fileNameOnBucket, Action<GetObjectResponse, string> result)
+        {
+            
+            resultTimeout = "";
+            Invoke("ResultTimeoutGetObjectBucket", timeoutGetObject);
+
+            var request = new GetObjectRequest
+            {
+                BucketName = S3BucketName,
+                Key = fileNameOnBucket
+            };
+
+            /* 
+            Client.GetObjectAsync(request, (responseObj) =>
+            {
+                var response = responseObj.Response;
+
+                if (response.ResponseStream != null)
+                {
+                    result?.Invoke(responseObj.Response, "");
+                    resultTimeout = "success";
+                }
+                else
+                    result?.Invoke(null, responseObj.Exception.ToString());
+                    
+            });
+            */
+            
+            
+            Client.GetObjectAsync(S3BucketName, fileNameOnBucket, (responseObj) =>
+            {
+                if (responseObj.Response.ResponseStream != null)
+                {
+                    using (var fs = System.IO.File.Create(@Application.persistentDataPath+@"/test.txt"))
+                    {
+                        byte[] buffer = new byte[81920];
+                        int count;
+                        while ((count = responseObj.Response.ResponseStream.Read(buffer, 0, buffer.Length)) != 0)
+                            fs.Write(buffer, 0, count);
+                        fs.Flush();
+                    }
+                }
+                }); 
+        }
+
+        /// <summary>
+        /// Post Object to S3 Bucket. 
+        /// </summary>
+        public void UploadObjectForBucket(string pathFile, string S3BucketName, string fileNameOnBucket, Action<PostObjectResponse, string> result)
+        {
+            if (!File.Exists(pathFile))
+            {
+                result.Invoke(null, "FileNotFoundException: Could not find file " + pathFile);
+                return;
+            }
+
+            var stream = new FileStream(pathFile, FileMode.Open, FileAccess.Read, FileShare.Read);
+            var request = new PostObjectRequest()
+            {
+                Bucket = S3BucketName,
+                Key = fileNameOnBucket,
+                InputStream = stream,
+                CannedACL = S3CannedACL.Private,
+                Region = S3Region
+            };
+
+            Client.PostObjectAsync(request, (responseObj) =>
+            {
+                if (responseObj.Exception == null)
+                    result.Invoke(responseObj.Response, "");
+                else
+                    result.Invoke(null, responseObj.Exception.ToString());
+            });
+        }
+
+        /// <summary>
+        /// Delete Objects in S3 Bucket
+        /// </summary>
+        public void DeleteObjectOnBucket(string fileNameOnBucket, string S3BucketName, Action<DeleteObjectsResponse, string> result)
+        {
+            List<KeyVersion> objects = new List<KeyVersion>();
+            objects.Add(new KeyVersion()
+            {
+                Key = fileNameOnBucket
+            });
+
+            var request = new DeleteObjectsRequest()
+            {
+                BucketName = S3BucketName,
+                Objects = objects
+            };
+
+            Client.DeleteObjectsAsync(request, (responseObj) =>
+            {
+                if (responseObj.Exception == null)
+                    result.Invoke(responseObj.Response, "");
+                else
+                    result.Invoke(null, responseObj.Exception.ToString());
+            });
+        }
+
+        #endregion 
+
+        #region METHODS UTILS
+
+        private void ResultTimeoutGetObjectBucket()
+        {
+            if(string.IsNullOrEmpty(resultTimeout))
+            {
+                OnResultGetObject.Invoke(null, "Timeout GetObject");
+            }
+        }
+
+        #endregion
+    }
+}
