3f4236a71b93af87de4a61ca5c45bfd31f95ead0
Kholstein <40220033+Kholstein@users.noreply.github.com>
2019-05-06 21:42:00 -0400

    Updated to Unity 2019

---
+++ AR Core Navigator/Assets/scripts/ColorPOI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPOI : MonoBehaviour
+{
+    public Color myColor;
+
+    void Update()
+    {
+        foreach (Transform child in gameObject.transform)
+        {
+            foreach (Transform grandchild in child.transform)
+            {
+                if (null == grandchild)
+                {
+                    continue;
+                }
+                else
+                {
+                    foreach (Transform greatgrandchild in grandchild.transform)
+                    {
+                        if (null == greatgrandchild)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            foreach (Transform thisisstupid in greatgrandchild.transform)
+                            {
+                                if (null == thisisstupid)
+                                {
+                                    continue;                              
+                                }
+                                else if ( thisisstupid.gameObject.GetComponentInChildren<SpriteRenderer>().color == myColor)
+                                {
+                                    break;                              
+                                }
+                                else
+                                {
+                                    Debug.Log(thisisstupid.gameObject);
+                                    thisisstupid.gameObject.GetComponentInChildren<SpriteRenderer>().color = myColor;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- AR Core Navigator/Assets/scripts/MoveWithSlider.cs
+++ AR Core Navigator/Assets/scripts/MoveWithSlider.cs
@@ -5,77 +5,60 @@ using UnityEngine.UI;
  
 public class MoveWithSlider : MonoBehaviour
 {
-    public List<GameObject> Targets;
+   // public List<GameObject> Targets;
 
     public GameObject[] TargetsList;
     private float m_Speed;
     public Slider SliderDis;
+
+    public ColorPOI Map1, Map2, Map3;
+
+    private Color Save1, Save2, Save3;
     
     void Start()
     {
         m_Speed = 10.0f;
+        Save1 = Map1.myColor;
+        Save2 = Map2.myColor;
+        Save3 = Map3.myColor;
     }
 
     void Update()
     {
-        for(int i = 0; i < GameObject.FindGameObjectsWithTag("POIMarker").Length; i++)
+        TargetsList = GameObject.FindGameObjectsWithTag("Map");
+        if(SliderDis.value == 0)
         {
-            if (!Targets.Contains(GameObject.FindGameObjectsWithTag("POIMarker")[i]))
-            {
-                Targets.Add(GameObject.FindGameObjectsWithTag("POIMarker")[i]);
-            }
+            Map1.myColor = Color.black;
+            Map2.myColor = Color.black;
+            Map3.myColor = Color.black;
         }
-
-        for(int i = 0; i < GameObject.FindGameObjectsWithTag("Building").Length; i++)
+        else
         {
-            if (!Targets.Contains(GameObject.FindGameObjectsWithTag("Building")[i]))
-            {
-                Targets.Add(GameObject.FindGameObjectsWithTag("Building")[i]);
-            }
+            Map1.myColor = Save1;
+            Map2.myColor = Save2;
+            Map3.myColor = Save3;
         }
-
-        for(int i = 0; i < GameObject.FindGameObjectsWithTag("Bus Route").Length; i++)
-        {
-            if (!Targets.Contains(GameObject.FindGameObjectsWithTag("Bus Route")[i]))
-            {
-                Targets.Add(GameObject.FindGameObjectsWithTag("Bus Route")[i]);
-            }
-        }
-
-        for(int i = 0; i < GameObject.FindGameObjectsWithTag("Taxi").Length; i++)
-        {
-            if (!Targets.Contains(GameObject.FindGameObjectsWithTag("Taxi")[i]))
-            {
-                Targets.Add(GameObject.FindGameObjectsWithTag("Taxi")[i]);
-            }
-        }
-
-        TargetsList = Targets.ToArray();
     }
     
     public void OnChange()
     {
         for(int i = 0; i < TargetsList.Length; i++)
         {
-            if(TargetsList[i].gameObject.tag == "POIMarker" || TargetsList[i].gameObject.tag == "Building")
+            if(TargetsList[i].gameObject.name == "Map")
             {
-                TargetsList[i].transform.position = new Vector3(TargetsList[i].transform.position.x, SliderDis.value, TargetsList[i].transform.position.z);
+                TargetsList[i].transform.position = new Vector3(TargetsList[i].transform.position.x, SliderDis.value*2, TargetsList[i].transform.position.z);
             }
-            if(TargetsList[i].gameObject.tag == "Bus Route")
+            if(TargetsList[i].gameObject.name == "Map 2")
             {
                 TargetsList[i].transform.position = new Vector3(TargetsList[i].transform.position.x, SliderDis.value*5.5f, TargetsList[i].transform.position.z);
             }
-            if(TargetsList[i].gameObject.tag == "Taxi")
+            if(TargetsList[i].gameObject.name == "Map 3")
             {
                 TargetsList[i].transform.position = new Vector3(TargetsList[i].transform.position.x, SliderDis.value*10, TargetsList[i].transform.position.z);
             }
             if (TargetsList[i].transform.position.y <= 0)
             {
                 TargetsList[i].transform.position = new Vector3(TargetsList[i].transform.position.x, 0.1f, TargetsList[i].transform.position.z);
-            }
-          //  if (TargetsList[i].transform.position.y >= 20)
-            {
-          //      Targets[i].transform.position = new Vector3(TargetsList[i].transform.position.x, 0.1f, TargetsList[i].transform.position.z);
             }
         }
     }
